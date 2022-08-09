5355ed0b79afd303bc7568b28ff49d76c60710c1
Sarang S <iam.sarangs@gmail.com>
2022-08-09 21:55:47 +0530

    feat: tasks completed

--- scripts/viteify.js
+++ scripts/viteify.js
@@ -1,5 +1,12 @@
-const { exec } = require('child_process');
-const { stat, renameSync, appendFile, unlink, writeFile } = require('fs');
+const { execSync } = require('child_process');
+const {
+  stat,
+  renameSync,
+  appendFile,
+  readFile,
+  unlink,
+  writeFile,
+} = require('fs');
 const { resolve } = require('path');
 const { readdir } = require('fs').promises;
 
@@ -15,38 +22,87 @@ async function* getFiles(dir) {
   }
 }
 
-;(async () => {
-    for await (const f of getFiles('./src')) {
-        if(f.endsWith(".js")){
-            renameSync(f,f.slice(0,-3).concat(".jsx"))
-        }
+(async () => {
+  for await (const f of getFiles('./src')) {
+    if (f.endsWith('.js')) {
+      renameSync(f, f.slice(0, -3).concat('.jsx'));
     }
+  }
 
-    await stat('./public/index.html', (err,stats) => {
-        if(!err){
-            unlink('./public/index.html',(err) => {
-                if(err) throw err
-            })
-        }
-    })
+  let packageManager = 'npm';
+  stat('yarn.lock', (err, stats) => {
+    if (!err) {
+      packageManager = 'yarn';
+    }
+  });
 
-    await appendFile('index.html', '<script type="module" src="./src/index.jsx"></script>', (err) => {
-        if (err) throw err
-    })
+  stat('./public/index.html', (err, stats) => {
+    if (!err) {
+      unlink('./public/index.html', err => {
+        if (err) throw err;
+      });
+    }
+  });
 
-    await appendFile('vite.config.js', '', (err) => {
-        if (err) throw err
-    })
+  appendFile(
+    'index.html',
+    '<script type="module" src="./src/index.jsx"></script>',
+    err => {
+      if (err) throw err;
+    }
+  );
 
-    await exec('npm install vite @vitejs/plugin-react --save-dev', (err) => {
-        if (err) throw err
-    })
+  appendFile('vite.config.js', '', err => {
+    if (err) throw err;
+  });
 
-    let package = JSON.parse(await readFile('./package.json'))
-    package.scripts.start = 'vite'
-    package.scripts.build = 'vite build --outDir build'
+  if (packageManager == 'npm') {
+    execSync('npm i vite @vitejs/plugin-react --save-dev', {
+      stdio: [0, 1, 2],
+    });
+  } else if (packageManager == 'yarn') {
+    execSync('yarn add vite @vitejs/plugin-react -D', { stdio: [0, 1, 2] });
+  }
 
-    await writeFile('./package.json', JSON.stringify(file, null, 2),err => {if (err) throw err})
+  readFile('./package.json', (err, data) => {
+    if (err) throw err;
+    let package = JSON.parse(data);
+    package.scripts.start = 'vite';
+    package.scripts.build = 'vite build --outDir build';
 
-      
-  })()
+    writeFile('./package.json', JSON.stringify(package, null, 2), err => {
+      if (err) throw err;
+    });
+  });
+
+  console.log(
+    'Convert all process.env to import.meta.env as its how vite exposes environment variables. Refer to https://vitejs.dev/guide/env-and-mode.html'
+  );
+
+  const readline = require('readline');
+  const prompt = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  prompt.question('Perform PWA migration to Vite?[Y/n]', answer => {
+    if (answer == 'Y') {
+      if (packageManager == 'npm') {
+        execSync('npm i vite-plugin-pwa -D', { stdio: [0, 1, 2] });
+      } else if (packageManager == 'yarn') {
+        execSync('yarn add vite-plugin-pwa -D', { stdio: [0, 1, 2] });
+      }
+
+      let pwa = `import { VitePWA } from 'vite-plugin-pwa'
+export default defineConfig({
+    plugins: [
+        VitePWA({ registerType: 'autoUpdate' })
+    ]
+})
+            `;
+      appendFile('vite.config.js', pwa, err => {
+        if (err) throw err;
+      });
+    }
+    prompt.close();
+  });
+})();
